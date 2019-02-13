8f1964f6b6a695d7767f684aa0e8e4c059697194
Anna Karutina <anna.karutina@khk.ee>
2019-02-13 14:34:18 +0200

    condition - if - else lause

---
+++ php_alused/condition/index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna.karutina
+ * Date: 13.02.2019
+ * Time: 14:14
+ */
+$eesnimi = 'Anna';
+$perenimi = 'Karutina';
+$vanus = 38;
+$kaal = 53.4;
+$sugu = 'naine';
+
+if($sugu == 'mees'){
+    echo '<p style="color: blue;">';
+} else {
+    echo '<p style="color: red;">';
+}
+echo 'Minu eesnimi on '.$eesnimi.'<br>';
+echo 'Minu perenimi on '.$perenimi.'<br>';
+echo 'Olen '.$vanus.' aastat vana<br>';
+echo 'Kaalun '.$kaal.' kg<br>';
+echo '</p>';
+
