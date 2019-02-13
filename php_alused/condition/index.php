5f9b8df80a24abb5cc3a3f08ee546fe2511a7c7d
Anna Karutina <anna.karutina@khk.ee>
2019-02-13 15:05:08 +0200

    condition - paarisarvu värviline väljastamine

--- php_alused/condition/index.php
+++ php_alused/condition/index.php
@@ -1,21 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: anna.karutina
- * Date: 13.02.2019
- * Time: 14:14
- */
-$eesnimi = 'Anna';
-$perenimi = 'Karutina';
-$vanus = 38;
-$kaal = 53.4;
-$sugu = 'naine';
-
-echo ($sugu == 'mees') ? '<p style="color: blue">' : '<p style="color: red">';
-
-echo 'Minu eesnimi on '.$eesnimi.'<br>';
-echo 'Minu perenimi on '.$perenimi.'<br>';
-echo 'Olen '.$vanus.' aastat vana<br>';
-echo 'Kaalun '.$kaal.' kg<br>';
-echo '</p>';
+// genereerime juhuslik täisarv vahemikus 0 kuni 100
+$arv = rand(0, 100);
+// arvutame jääk 2-ga jagamisel
+$jaak = $arv % 2;
+// kontrollime kui jääk on 0 - paaris arv
+if($jaak == 0){
+    echo '<div class="paaris">'.$arv.'</div>';
+} else {
+    echo '<div class="paaritu">'.$arv.'</div>';
+}
 
