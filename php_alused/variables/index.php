853f752c70a34080bc19eef68ad52dcf436c23e9
Anna Karutina <anna.karutina@khk.ee>
2019-02-13 14:21:25 +0200

    variables - defineeriti muutujad

---
+++ php_alused/variables/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna.karutina
+ * Date: 13.02.2019
+ * Time: 14:14
+ */
+$eesnimi = 'Anna';
+$perenimi = 'Karutina';
+$vanus = 38;
+$kaal = 53.4;
+
+//var_dump($eesnimi); // muutuja sisu test vaatamine
