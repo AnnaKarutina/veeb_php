30281e9755cc464b96f47f0b0e3cb81a8672f3d5
Anna Karutina <anna.karutina@khk.ee>
2019-02-25 09:28:58 +0200

    array - massiivitöötluse ülesanne

--- php_alused/array/index.php
+++ php_alused/array/index.php
@@ -1,49 +1,9 @@
 <?php
-$peppa = array(
-    'nimi' => 'Peppa',
-    'sugu' => 'naine',
-    'vanus' => 4,
-    'pikkus' => 1.04
-);
-$george = array(
-    'nimi' => 'George',
-    'sugu' => 'mees',
-    'vanus' => 2,
-    'pikkus' => 0.95
-);
-
-foreach ($peppa as $nimi=>$vaartus){
-    echo $nimi.' - '.$vaartus.'<br>';
-}
-echo '<hr>';
-
-foreach ($george as $nimi=>$vaartus){
-    echo $nimi.' - '.$vaartus.'<br>';
-}
-echo '<hr>';
-
-echo $peppa['nimi'].' on '.$peppa['vanus'].' aastat vana<br>';
-echo $george['nimi'].' on '.$george['vanus'].' aastat vana<br>';
-echo '<hr>';
-
-$porsad = array();
-$porsad['peppa'] = $peppa;
-$porsad['george'] = $george;
-
-$porsad['peppa']['lemmik varv'] = 'punane';
-$porsad['george']['lemmik varv'] = 'sinine';
-
-foreach ($porsad as $porsaseNimi=>$porsaseAndmed){
-    if($porsaseAndmed['sugu'] == 'naine'){
-        echo '<p style="color: red">';
-    } else {
-        echo '<p style="color: blue">';
-    }
-    echo '<b>'.$porsaseNimi.'</b></p>';
-    echo '<ul>';
-    foreach ($porsaseAndmed as $nimetus=>$vaartus){
-        echo '<li>'.$nimetus.' - '.$vaartus.'</li>';
-    }
-    echo '</ul>';
-}
-
+// Ülesanne massiivitöötlusest
+/*
+a)Loo massiiv raamatute admete hoidmiseks - inspireeri Apollo või rahvaraamatu poodidest :)
+b)Väljasta antud massiiv test kujul andmete kontrollimiseks
+c)Loo funktsioon, mis väljastab massiiv tabeli kujul, kus eraldi rida on ühe raamatud andmed
+NB! Mitte unustada tabeli päisest!
+d) Mõtle, mis moodi oleks võimalik sorteerida antud andmestik näiteks hinne järgi ja loo vastav funktsioon
+e) Mõtle, mis moodi oleks võimalik sorteerida antud andmestik näiteks kategooria järgi ja loo vastav funktsioon
