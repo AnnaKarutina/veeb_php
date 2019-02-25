836ea36878e6bed185336159777863e5afbdd1d8
Anna Karutina <anna.karutina@khk.ee>
2019-02-25 09:13:45 +0200

    array - sidestatud massiivi läbimine foreach abil

--- php_alused/array/index.php
+++ php_alused/array/index.php
@@ -12,13 +12,33 @@ $george = array(
     'pikkus' => 0.95
 );
 
+foreach ($peppa as $nimi=>$vaartus){
+    echo $nimi.' - '.$vaartus.'<br>';
+}
+echo '<hr>';
+
+foreach ($george as $nimi=>$vaartus){
+    echo $nimi.' - '.$vaartus.'<br>';
+}
+echo '<hr>';
+
 echo $peppa['nimi'].' on '.$peppa['vanus'].' aastat vana<br>';
 echo $george['nimi'].' on '.$george['vanus'].' aastat vana<br>';
+echo '<hr>';
 
 $porsad = array();
 $porsad['peppa'] = $peppa;
 $porsad['george'] = $george;
-echo '<pre>';
-print_r($porsad);
-echo '</pre>';
+
+$porsad['peppa']['lemmik varv'] = 'punane';
+$porsad['george']['lemmik varv'] = 'sinine';
+
+foreach ($porsad as $porsaseNimi=>$porsaseAndmed){
+    echo '<b>'.$porsaseNimi.'</b><br>';
+    echo '<ul>';
+    foreach ($porsaseAndmed as $nimetus=>$vaartus){
+        echo '<li>'.$nimetus.' - '.$vaartus.'</li>';
+    }
+    echo '</ul>';
+}
 
